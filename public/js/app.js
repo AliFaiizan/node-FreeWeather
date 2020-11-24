be5950dd091f903c325e8eb180ab06a0d31b524c
ALI-DESKTOP\Ali <alifaizanahmad@gmail.com>
2020-11-24 11:38:54 +0500

    Initial Commit

---
+++ public/js/app.js
@@ -0,0 +1,43 @@
+
+
+console.log('Client side javascript file is loaded!')
+
+
+const locationForm=document.querySelector('form')
+const locationData=document.querySelectorAll('input')[0].value
+
+
+locationForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const locationData=document.querySelectorAll('input')[0].value;
+    console.log(locationData);
+    const day=document.querySelectorAll('.day')[0]
+    const date=document.querySelectorAll('.date')[0]
+    const location=document.querySelectorAll('.location')[0]
+    const ImgUrl=document.querySelectorAll('.locationforecast-icon img')[0] //pending
+    const Tempurature=document.querySelectorAll('.num')[0]
+    const windSpeed=document.querySelectorAll('.wind')[0]
+    const windDirection=document.querySelectorAll('.direction')[0]
+    const humidity=document.querySelectorAll('.umbrella')[0]
+
+    var d= new Date()
+    var weekDay=['Sunday','Monday','Tuesday','Wednesday','Thrusday','Friday','Saturday']
+    var Month=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+    const nday=weekDay[d.getDay()]
+    const ndate=d.getDate()
+    const nmonth=Month[d.getMonth()]
+
+
+    fetch('http://localhost:3000/weather?address='+locationData).then((response)=>{
+   response.json().then((data)=>{
+       console.log(data.main.temp)
+       day.textContent=nday
+       date.textContent=ndate+' '+nmonth
+       location.textContent=data.name
+       Tempurature.innerHTML=data.main.temp+'<sup>o</sup>C'
+       windSpeed.innerHTML='<img  src="images/icon-wind.png" alt="no image found"></img>'+data.wind.speed+'Km/h'
+       windDirection.innerHTML='<img  src="images/icon-compass.png" alt="no image found">'+data.wind.deg+'<sup>o</sup>'
+       humidity.innerHTML='<img src="images/icon-umberella.png" alt="no image found">'+data.main.humidity
+   })
+})
+})
---
+++ src/app.js
@@ -0,0 +1,115 @@
+const path = require('path')
+const express = require('express')
+const hbs=require('hbs')
+const https= require('https')
+
+
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialPath= path.join(__dirname,'../templates/partials')
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+hbs.registerPartials(partialPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Andrew Mead'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    const address=req.query.address
+    if(!req.query.address){
+        return res.send({
+            error:'You have not provided any address',
+        })
+    }
+  
+
+        var url="https://api.openweathermap.org/data/2.5/weather?q="+ address +"&appid=f8b38007cd9048407505063a677e44c6&units=metric";
+      
+        https.get(url,(response)=>{
+      
+            console.log(response.statusCode);
+      
+            response.on("data",(data)=>{
+      
+            const weatherData= JSON.parse(data); //JSON.stringify(data)
+            console.log(weatherData)
+            // const temp = weatherData.main.temp;
+            // const weatherDiscription = weatherData.weather[0].description;
+            // const feelLike = weatherData.main.feels_like;
+            // const imgUrl="http://openweathermap.org/img/wn/"+ weatherData.weather[0].icon +"@2x.png"
+            
+            // const forecast={
+            //     forecast:weatherData
+            //     // weatherDiscription:weatherDiscription,
+            //     // imgUrl:imgUrl,
+            //     // feelLike:feelLike,
+            //     // location:address,
+            //     // temp:temp
+            // }
+            
+            res.send(weatherData)
+          })
+          // console.log(JSON.parse(response))
+      
+      
+        })
+    
+})
+
+app.get('/contact',(req,res)=>{
+
+    res.render('contact',{
+
+    })
+
+})
+
+
+
+
+app.get('/help/*',(req,res)=>{
+    res.render('Error404',{
+        title:'Page not Found',
+        description: ' Resquested page is not found'
+    })
+})
+
+
+app.get('*',(req,res)=>{
+    res.render('Error404',{
+        title: 'Error 404 -- Page Not Found',
+        description: 'The page that you have requested is not found',
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
+
+ 
